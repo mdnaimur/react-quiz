d716e21dc38ca16a1be66f8a7b04a655a8be5fa0
mdnaimurr <mdnaimurr@gmail.com>
2022-08-09 10:03:57 +0600

    routing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import "../styles/App.css";
 import Layout from "./Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
-// import Quiz from "./pages/Quiz";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
-// import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+
 
 function App() {
   return (
-    <>
+    <Router>
       <Layout >
-        {/* <Home /> */}
-
-        {/* <Signup /> */}
-
-        {/* <Login /> */}
-
-        {/* <Quiz /> */}
-
-        <Result />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+          <Route path="quiz" element={<Quiz />} />
+          <Route path="result" element={<Result />} />
+        </Routes>
       </Layout>
-    </>
+    </Router>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from "../assets/images/logo-bg.png";
 import classes from '../styles/Nav.module.css';
 import Account from './Account';
-
 
 export default function Nav() {
     return (
         <nav className={classes.nav}>
             <ul>
                 <li>
-                    <a href="index.html" className={classes.brand}>
+                    <Link to="/" className={classes.brand}>
                         <img src={logo} alt="Learn with Self Logo" />
                         <h3>Learn With self</h3>
-                    </a>
+                    </Link>
                 </li>
             </ul>
             <Account />

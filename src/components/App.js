cf37cdbce01821afeae56a03e2738aa7285db27b
mdnaimurr <mdnaimurr@gmail.com>
2022-08-09 08:50:38 +0600

    Result page done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import "../styles/App.css";
 import Layout from "./Layout";
-import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+// import Quiz from "./pages/Quiz";
 // import Login from "./pages/Login";
 // import Signup from "./pages/Signup";
 // import Home from "./pages/Home";
@@ -15,7 +16,9 @@ function App() {
 
         {/* <Login /> */}
 
-        <Quiz />
+        {/* <Quiz /> */}
+
+        <Result />
       </Layout>
     </>
   );
